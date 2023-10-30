e67a804376dbb1bc5e436dab296dbd1f7c1e6c47
Alysha Huot <alyshajhuot@gmail.com>
2023-10-30 16:38:42 -0600

    complete lightcoin assignment

--- index.js
+++ index.js
@@ -1,13 +1,61 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
 
-  constructor(amount) {
+  get balance() {
+    let total = 0;
+    for (let transaction of this.transactions) {
+      total += transaction.value;
+    }
+    return total;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+
+}
+
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+      this.time = new Date();
+      this.account.addTransaction(this);
+  }
+
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return this.amount * -1;
+  }
+
+  isAllowed() {
+    return this.amount <= this.account.balance;
+  }
+
+  commit() {
+    if (this.isAllowed()){
+      this.time = new Date();
+      this.account.addTransaction(this);
+    }
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
   }
 
 }
@@ -17,13 +65,26 @@ class Withdrawal {
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("snow-patrol");
 
-t1 = new Withdrawal(50.25);
+ta = new Deposit(75.00, myAccount);
+ta.commit();
+console.log("ta", ta);
+console.log("balance1:", myAccount.balance);
+
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
+console.log("balance2:", myAccount.balance);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
+console.log("balance3", myAccount.balance);
 
-console.log('Balance:', balance);
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log("transaction 3:", t3);
+console.log("balance4", myAccount.balance);
+
+console.log("balance", myAccount.balance);
